35334c454373c8680e916d10ba4a12db49b92d88
Sabaniki <sawada13@mccc.jp>
2021-02-20 16:23:04 +0900

    インターフェイスが取れる様になりました。anyhowもなんか使えてます。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+mod util;
+mod interface;
+
+#[macro_use]
+extern crate log;
+
+use std::env;
+use util::app::get_arg;
+use std::process::exit;
+
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let interface_name = get_arg().unwrap_or_else(|e| {
+        error!("{}", e);
+        "".to_string()
+    });
+
+    let interface = interface::get_from_name(interface_name)
+        .unwrap_or_else(|e|{
+            error!("{}", e);
+            exit(-1);
+        });
+    print!("{:?}", interface);
 }
