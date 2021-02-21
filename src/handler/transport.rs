56a9e8c98e18769a2028a12e9521b69a7d1329e1
Sabaniki <sawada13@mccc.jp>
2021-02-22 00:25:12 +0900

    表示する部分が書けた

--- src/handler/transport.rs
+++ src/handler/transport.rs
@@ -2,7 +2,8 @@ use crate::packet::{GettableEndPoints, print_packet_info};
 use pnet::packet::tcp::TcpPacket;
 use pnet::packet::udp::UdpPacket;
 
-// TCPパケットを構築する
+// TCPパケットを構築する。
+//引数のパケットにはIPパケットが入り、それの皮を剥いたものを次に渡す。
 pub fn tcp_handler(packet: &dyn GettableEndPoints) {
     let tcp = TcpPacket::new(packet.get_payload());
     if let Some(tcp) = tcp {
@@ -11,6 +12,7 @@ pub fn tcp_handler(packet: &dyn GettableEndPoints) {
 }
 
 //UDPパケットを構築する
+//引数のパケットにはIPパケットが入り、それの皮を剥いたものを次に渡す。
 pub fn udp_handler(packet: &dyn GettableEndPoints) {
     let udp = UdpPacket::new(packet.get_payload());
     if let Some(udp) = udp {
--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,9 @@
+use log::{info};
+
 pub mod ip;
 pub mod transport;
+
+const WIDTH: usize = 20;
 
 pub trait GettableEndPoints {
     fn get_source(&self) -> String;
@@ -7,6 +11,41 @@ pub trait GettableEndPoints {
     fn get_payload(&self) -> &[u8];
 }
 
-pub fn print_packet_info(l3: &dyn GettableEndPoints, l4: &dyn GettableEndPoints, proto: &str) {
-    //TODO: 中身の実装
+// アプリケーション層ので0田をバイナリで表示する
+pub fn print_packet_info(l3: &dyn GettableEndPoints, l4: &dyn GettableEndPoints, protocol: &str) {
+    info!("{}", format!(
+        "Captured a {} packet from {} | {} to {} | {}\n",
+        protocol,
+        l3.get_source(),
+        l4.get_source(),
+        l3.get_destination(),
+        l4.get_destination()
+    ));
+
+    let payload = l4.get_payload();
+    let len = payload.len();
+
+    // ペイロードの表示
+    // 指定した定数幅で表示を行う
+    for i in 0..len {
+        // {:<02X} ← "左詰めで2桁の16進数で表わせ"というフォーマットを指定している
+        print!("{:<02X}", payload[i]);
+        if i % WIDTH == WIDTH - 1 || i == len - 1 {
+            for _ in 0..WIDTH - 1 - (i % WIDTH) {
+                print!("{}", " ".repeat(3));
+            }
+            print!("| ");
+            for j in i - i % WIDTH..=i {
+                if payload[j].is_ascii_alphabetic() {
+                    print!("{}", payload[j] as char);
+                } else {
+                    // 非ascii文字は?として表示
+                    print!("?");
+                }
+            }
+            println!();
+        }
+    }
+    println!("{}", "=".repeat(WIDTH * 3));
+    println!();
 }
