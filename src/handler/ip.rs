b62d340177d5be0273d4cf63e333cd99e08d613a
Sabaniki <sawada13@mccc.jp>
2021-02-22 16:04:01 +0900

    重複部分を切り出し

--- src/handler/ip.rs
+++ src/handler/ip.rs
@@ -1,40 +1,35 @@
 use pnet::packet::ethernet::EthernetPacket;
-use pnet::packet::ip::IpNextHeaderProtocols;
+use pnet::packet::ip::{IpNextHeaderProtocols, IpNextHeaderProtocol};
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::Packet;
 use crate::handler::transport::{tcp_handler, udp_handler};
+use crate::packet::GettableEndPoints;
 
 // IPv4パケットを構築し、次のレイヤのハンドラを呼び出す
 pub fn v4_handler(ethernet: &EthernetPacket) {
     if let Some(packet) = Ipv4Packet::new(ethernet.payload()) {
-        match packet.get_next_level_protocol() {
-            IpNextHeaderProtocols::Tcp => {
-                tcp_handler(&packet);
-            }
-            IpNextHeaderProtocols::Udp => {
-                udp_handler(&packet);
-            }
-            _ => {
-                info!("This packet is neither TCP nor UDP");
-            }
-        }
+        call_transport_handler(&packet, packet.get_next_level_protocol());
     }
 }
 
 // IPv6パケットを構築し、次のレイヤのハンドラを呼び出す
 pub fn v6_handler(ethernet: &EthernetPacket) {
     if let Some(packet) = Ipv6Packet::new(ethernet.payload()) {
-        match packet.get_next_header() {
-            IpNextHeaderProtocols::Tcp => {
-                tcp_handler(&packet);
-            }
-            IpNextHeaderProtocols::Udp => {
-                udp_handler(&packet);
-            }
-            _ => {
-                info!("This packet is neither TCP nor UDP");
-            }
+        call_transport_handler(&packet, packet.get_next_header());
+    }
+}
+
+fn call_transport_handler(packet: &dyn GettableEndPoints,next: IpNextHeaderProtocol) {
+    match next {
+        IpNextHeaderProtocols::Tcp => {
+            tcp_handler(packet);
+        },
+        IpNextHeaderProtocols::Udp => {
+            udp_handler(packet);
+        },
+        _ => {
+            info!("This packet is neither TCP nor UDP");
         }
     }
 }
